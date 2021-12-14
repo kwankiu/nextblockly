ff17f8a1a7a1ca0ce417a96c9579c0f0de947d58
Samuel <ngkwankiuu@gmail.com>
2021-12-14 18:37:26 +0800

    nextjs

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import React from 'react';
+
+import BlocklyComponent, { Block, Value, Field, Shadow } from './Blockly';
+
+import BlocklyJS from 'blockly/javascript';
+
+import './blocks/customblocks';
+import './generator/generator';
+
 export default function Home() {
+
+  const simpleWorkspace = React.createRef();
   return (
     <div className={styles.container}>
       <Head>
@@ -16,40 +27,43 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <BlocklyComponent ref={simpleWorkspace}
+          readOnly={false} trashcan={true} media={'media/'}
+          move={{
+            scrollbars: true,
+            drag: true,
+            wheel: true
+          }}
+          initialXml={`
+<xml xmlns="http://www.w3.org/1999/xhtml">
+<block type="controls_ifelse" x="0" y="0"></block>
+</xml>
+      `}>
+            <Block type="test_react_field" />
+            <Block type="test_react_date_field" />
+            <Block type="controls_ifelse" />
+            <Block type="logic_compare" />
+            <Block type="logic_operation" />
+            <Block type="controls_repeat_ext">
+              <Value name="TIMES">
+                <Shadow type="math_number">
+                  <Field name="NUM">10</Field>
+                </Shadow>
+              </Value>
+            </Block>
+            <Block type="logic_operation" />
+            <Block type="logic_negate" />
+            <Block type="logic_boolean" />
+            <Block type="logic_null" disabled="true" />
+            <Block type="logic_ternary" />
+            <Block type="text_charAt">
+              <Value name="VALUE">
+                <Block type="variables_get">
+                  <Field name="VAR">text</Field>
+                </Block>
+              </Value>
+            </Block>
+          </BlocklyComponent>
       </main>
 
       <footer className={styles.footer}>
